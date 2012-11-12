acf6cb44d568fcaa4244efbb74e25a650fb09b12
qluo1 <qi.dataingelligence@gmail.com>
2012-11-12 17:58:07 +1100

    working on sigon

--- unit2/unit2.py
+++ unit2/unit2.py
@@ -5,6 +5,7 @@ import os
 import cgi
 import datetime
 import webapp2
+import re
 
 from google.appengine.ext import db
 from google.appengine.api import users
@@ -15,6 +16,7 @@ ROOT=os.path.dirname(os.path.abspath(__file__))
 
 env = Environment(loader = FileSystemLoader(os.path.join(ROOT,"templates")))
 
+p = re.compile('^[_.0-9a-z-]+@([0-9a-z][0-9a-z-]+.)+[a-z]{2,4}$')
 def rot13(s):
 	"""
 	 Map upper case A-Z to N-ZA-M and lower case a-z to n-za-m
@@ -28,6 +30,21 @@ def rot13(s):
 	ret = ""
 	for i in s:
 		ret = ret + m.get(i,i)
+
+	return ret
+
+def validate_logon(name,passwd,password2,email):
+	"""
+	"""
+	ret = {'name_error': None, 'passwd_error': None, 'email_error': None}
+	if re.search("\s",name):
+		ret['name_error'] = 'invalid name'
+	elif passwd != passwrd2:
+		ret['passwd_error'] 'password doesnot match'
+	elif p.search(email) is None:
+		ret['email_error'] = 'invalid email'
+	else:
+		return None
 
 	return ret
 	
@@ -65,6 +82,34 @@ class Unit2_rot13(webapp2.RequestHandler):
 		temp = env.get_template("unit2.html")
 		self.response.out.write(temp.render(context))
 
+class Unit2_sigon(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['Content Type'] = "text/html"
+		temp = env.get_template("logon.html")
+		self.response.out.write(temp.render({}))	
+	def post(self):
+		name = self.request.get("name")
+		passwd = self.request.get("passwd")
+		passwd2 = self.request.get("passwd2")
+		email = self.request.get("email")
+		# validation
+		ret = validate_logon(name,passwd,passwd2,email)
+		
+		self.response.headers['Content Type'] = "text/html"
+		if ret:
+			temp = env.get_template("logon.html")
+			return self.response.out.write(temp.render(ret))	
+		else:
+			return redirect("/unit2/welcome?%s" % name)
+
+class Unit2_welcome(webapp2.RequestHandler):
+	def get(self):
+		self.response.headers['Content Type'] = "text/html"
+		name = self.request.get("name")
+		temp = env.get_template("welcome.html")
+		self.response.out.write(temp.render({'name':name}))
+
+
 class TestHandler(webapp2.RequestHandler):
 	def post(self):
 		q = self.request.get("q")
@@ -76,5 +121,7 @@ app = webapp2.WSGIApplication([
   ('/', MainPage),
   ('/unit2/rot13', Unit2_rot13),
   ('/testform', TestHandler),
+  ('/unit2/sigon', Unit2_sigon),
+  ('/unit2/welcome', Unit2_welcome),
 ], debug=True)
 
